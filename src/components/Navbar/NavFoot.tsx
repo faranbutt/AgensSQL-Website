d41ca61322c0e754eceb669c50a8aee942175beb
faranbutt <faranbutt789@gmail.com>
2023-07-24 16:30:29 +0500

    Pushing the frontend

--- src/components/Navbar/NavFoot.tsx
+++ src/components/Navbar/NavFoot.tsx
@@ -4,7 +4,7 @@
 export default function NavFoot() {
   return (
     <div className="p-10 flex justify-between items-center" style={{fontFamily:"Monda",fontWeight:'normal'}}>
-        <div><img src="./assets/logo.png" alt="Logo" width={220} height={160}/></div>
+        <div><img src="https://bitnine.net/wp-content/uploads/2021/02/b_logo.png" alt="Logo" width={220} height={160}/></div>
         <div className="flex gap-10 items-center">
             <div className={`text-[#fac433] hover:text-[#fac433] cursor-pointer`}>PRODUCTS</div>
             <div className={`hover:text-[#fac433] cursor-pointer`}>USE CASES</div>
