5687cdbb9d26c2921bce6a1cd4b907418cf84add
faranbutt <faranbutt789@gmail.com>
2023-07-24 16:25:11 +0500

    Pushing the frontend

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -4,7 +4,7 @@
 export default function Header() {
   return (
     <>    
-    <div className="bg-[url('./assets/banner.jpg')] h-10 text-center pt-1">
+    <div className="bg-[url('./images/banner.jpg')] h-10 text-center pt-1">
         {/* <img src={'/banner.jpg'} alt="banner" className="w-full"/> */}
         <div className="text-center text-white text-lg hover:text-yellow-500 " style={{fontFamily:"Monda",fontWeight:'normal'}}> <b>AG Cloud Express </b> : Experience graph database in a cloud environment for FREE!</div>
     </div>
